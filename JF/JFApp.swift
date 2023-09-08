6b802ec3653ea22e3b4446f4645ca209aac5da8a
Yaroslav Orlov <ulverforest@gmail.com>
2023-09-08 10:19:30 +0300

    Initial Commit

---
+++ JF/JFApp.swift
@@ -0,0 +1,17 @@
+//
+//  JFApp.swift
+//  JF
+//
+//  Created by Yaroslav Orlov on 08.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct JFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
